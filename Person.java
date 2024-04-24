271859bb8d9367eb6e286d39b7cbb24ab5c42dc1
chemod77 <51191713+ch3mod@users.noreply.github.com>
2024-04-24 10:49:05 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    public static void main(String[] args) { //fungsi utama di class
+ 
+        var person1 = new Person(); //kita buat objek bernama person1, semua atributnya diambil dari class Person
+ 
+        Person person2 = new Person(); //buat objek person2, semua atributnya diambil dari class Person
+ 
+        person1.ucapHalo("Aufa");
+        person2.ucapHalo("Hasan");
+ 
+    }
+}
---
+++ PersonApp.java
@@ -0,0 +1,6 @@
+public class PersonApp {
+    //deklarasi method di class Person
+    void ucapHalo(String namaOrang){
+        System.out.println("Halo " + namaOrang + ", Selamat Datang di blog pauzan.com");
+    }
+}
